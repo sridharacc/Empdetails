6d8f826af178ea5a766ec46c798d2bd3b3d11cb9
sridharacc <vkts.sridharcource@gmail.com>
2022-02-06 20:29:03 +0530

    open now

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,13 @@
+package org.emp;
+
+public class Employee {
+	private void empID(int Id,String Officecode) {
+		System.out.println("Employee Id-"+Id+"\n"+"office-code-"+Officecode);
+
+	}
+	public static void main(String[] args) {
+		Employee e= new Employee();
+		e.empID(1123, "Ct1234");
+	}
+
+}
